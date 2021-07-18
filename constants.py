d7b71baab7a3e3cc1009c64fce6fc7b52c3d1bc5
Sunny Cheung <sunnycheungabc@gmail.com>
2021-07-18 19:30:11 +0800

    minor changes

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-FRONTEND_URL = 'http://wykoj.owo.idv.hk'
+FRONTEND_URL = 'https://wykoj.owo.idv.hk'
 MAX_THREAD_NO = 14
 DEBUG = False
 CONFIG = {}
--- main.py
+++ main.py
@@ -68,4 +68,4 @@ if __name__ == '__main__':
     if constants.DEBUG:
         uvicorn.run(app, port=8000, host='0.0.0.0')
     else:
-        uvicorn.run(app, port=80, host='0.0.0.0')
+        uvicorn.run(app, port=8000, host='0.0.0.0')
